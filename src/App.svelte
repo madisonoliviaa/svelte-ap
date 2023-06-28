6c081ca18951372c29e80da1579c6009c887668c
madisonoliviaa <madisonoliviaa8@gmail.com>
2023-06-28 11:01:23 -0400

    added keyframe in style brackets & animation in h1

--- src/App.svelte
+++ src/App.svelte
@@ -5,19 +5,21 @@
 
 <h1>Madisons first svelte component</h1>
 
-<p>Hello world!</p>
+<p class="links">Hello world!</p>
 
-<div>
+<div class="links">
   <a href="https://twitter.com/madisonoliviaa_">Link to my twitter</a>
 </div>
 
-<Component2/>
+<Component2 />
 
 <style>
-  h1 {
-    color: rebeccapurple;
-  }
-  p {
-    font-size: 44px;
-  }
+  h1 {color: rebeccapurple; animation: zoom 30s infinite;}
+  p { font-size: 44px; }
+  .links {text-align: center;}
+@keyframes zoom {
+  0%, 100% {transform: scale(3)}
+  50% {transform: scale(0.5)}
+}
+  
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import App from './App.svelte';
-import Component2__SvelteComponent_ from './component2.svelte';
 
 var app = new App({
 	target: document.body
